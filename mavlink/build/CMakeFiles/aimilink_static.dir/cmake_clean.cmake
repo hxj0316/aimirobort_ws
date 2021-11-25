153711a48067e78b9373f6c266c02cf140f8203c
hxj0316 <305625579@qq.com>
2021-11-25 16:57:53 +0800

    aimibot_ws

---
+++ mavlink/build/CMakeFiles/aimilink_static.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aimilink_static.dir/src/linux_serial.cpp.o"
+  "CMakeFiles/aimilink_static.dir/src/mavlink_interface.cpp.o"
+  "lib/libaimilink.pdb"
+  "lib/libaimilink.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aimilink_static.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
